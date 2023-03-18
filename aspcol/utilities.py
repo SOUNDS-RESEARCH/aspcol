b3d62d8c699d8391ccf602863665b6305edd900c
Jesper Brunnstroem <jbrunnst@esat.kuleuven.ac.be>
2023-03-18 23:16:01 +0100

    added flatten and restack dict fucntions

--- aspcol/utilities.py
+++ aspcol/utilities.py
@@ -309,3 +309,32 @@ class EventCounter:
     def progress(self):
         self.idx += 1 
         self.check_events()
+
+
+
+
+def flatten_dict(dict_to_flatten, parent_key="", sep="~"):
+    items = []
+    for key, value in dict_to_flatten.items():
+        new_key = parent_key + sep + key if parent_key else key
+        if isinstance(value, dict):
+            items.extend(flatten_dict(value, new_key, sep).items())
+        else:
+            items.append((new_key, value))
+    new_dict = dict(items)
+    return new_dict
+
+
+def restack_dict(dict_to_stack, sep="~"):
+    """Only accepts dicts of depth 2.
+    All elements must be of that depth."""
+    extracted_data = {}
+    for multi_key in dict_to_stack.keys():
+        key_list = multi_key.split(sep)
+        if len(key_list) > 2:
+            raise NotImplementedError
+        if key_list[0] not in extracted_data:
+            extracted_data[key_list[0]] = {}
+        extracted_data[key_list[0]][key_list[1]] = dict_to_stack[multi_key]
+    return extracted_data
+
