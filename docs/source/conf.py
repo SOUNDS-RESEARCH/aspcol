4520c6616c967df7b8fb7f644c73ac32b886f456
Jesper Brunnstroem <jbrunnst@esat.kuleuven.ac.be>
2023-09-13 16:38:18 +0200

    changed docs to RTD theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -57,7 +57,8 @@ templates_path = ['_templates']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+#html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 
 
 # Add any paths that contain custom static files (such as style sheets) here,
