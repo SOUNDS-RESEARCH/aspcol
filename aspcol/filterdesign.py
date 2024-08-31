53856741cd21dbafc1678576dd7dce8e6c771eb2
Jesper Brunnstroem <jbrunnst@esat.kuleuven.ac.be>
2024-08-31 21:53:45 +0200

    moved many functions to new module fouriertransform

--- aspcol/filterdesign.py
+++ aspcol/filterdesign.py
@@ -8,147 +8,57 @@ import scipy.signal as signal
 import itertools as it
 import matplotlib.pyplot as plt
 
-
-# ================== BASIC DFT FUNCTIONS =============================
-
-def get_frequency_values(num_freq, samplerate):
-    """Deprecated, use get_real_freqs instead
-    
-    Get the frequency values of all positive frequency bins in Hz
-    numFreq is the number of frequency bins INCLUDING negative frequencies
-    If numFreq is even, it will return (numFreq/2)+1 values
-    If numFreq is odd, it will return (numFreq+1)/2 values
-    use np.fft.fftfreq to double check values when implementing. 
-    """
-    print(f"get_frequency_values is deprecated, use get_real_freqs instead")
-    if num_freq % 2 == 0:
-        return (samplerate / (num_freq)) * np.arange(num_freq // 2 + 1)
-    elif num_freq % 2 == 1:
-        raise NotImplementedError
-    else:
-        raise ValueError
-
-def get_freqs(num_freq : int, samplerate : int):
-    """Returns the sampled frequencies in Hz for a discrete Fourier transform
-
-    Parameters
-    ----------
-    num_freq : int
-        should equal the length of the sequence 
-        so it includes the number of negative frequencies
-    samplerate : int
-
-    Returns
-    -------
-    freqs : ndarray of shape (num_freq,)    
-    """
-    return np.arange(num_freq) * samplerate / num_freq
-
-def get_wavenum(num_freq : int, samplerate : int, c : float):
-    """Returns the wave numbers associated with the sampled frequencies of the DFT
-
-    See documentation for get_freqs
-    """
-    return get_angular_freqs(num_freq, samplerate) / c
-
-def get_angular_freqs(num_freq : int, samplerate : int):
-    """Returns the angular frequencies associated with the sampled frequencies of the DFT
-
-    See documentation for get_freqs
-    """
-    return 2 * np.pi * get_freqs(num_freq, samplerate)
-
-def get_real_freqs(num_freq : int, samplerate : int):
-    """Returns the real sampled frequencies in Hz for a discrete Fourier transform
-
-    Parameters
-    ----------
-    num_freq :int 
-        should equal the length of the sequence, so it includes 
-        the number of negative frequencies
-    samplerate : int
-    
-    Returns
-    -------
-    freqs : ndarray of shape (num_real_freq,)
-        if num_freq is even, num_real_freq = num_freq // 2 + 1
-        if num_freq is odd, num_real_freq = (num_freq + 1) // 2
-    """
-    if num_freq % 2 == 0:
-        return (samplerate / (num_freq)) * np.arange(num_freq // 2 + 1)
-    elif num_freq % 2 == 1:
-        raise NotImplementedError
-    else:
-        raise ValueError
-
-def get_real_wavenum(num_freq : int, samplerate : int, c : float):
-    """Get wave numbers associated with the real frequencies of the DFT
-
-    See documentation for get_real_freqs
-    """
-    return get_real_angular_freqs(num_freq, samplerate) / c
-
-def get_real_angular_freqs(num_freq : int, samplerate : int):
-    """Returns angular frequencies associated with the real frequencies of the DFT
-
-    See documentation for get_real_freqs
-    """
-    return 2 * np.pi * get_real_freqs(num_freq, samplerate)
-
-
-def insert_negative_frequencies(freq_signal, even):
-    """Inserts all negative frequency values under the assumption of conjugate symmetry, i.e. a real impulse response.
-
-    To be used in conjunction with get_real_freqs
-    
-    Parameters
-    ----------
-    freq_signal : ndarray of shape (num_real_freq, ...)
-    even : bool
-        if True, the full dft length was even
-
-    Returns
-    -------
-    freq_signal_full : ndarray of shape (num_freq, ...)
-        num_freq is even if even=True, and odd if even=False
-    
-    """
-    if even:
-        return np.concatenate(
-            (freq_signal, np.flip(freq_signal[1:-1, :, :].conj(), axis=0)), axis=0
-        )
-    else:
-        raise NotImplementedError
-    
-
-
-def dft_vector (freq_idx : int, dft_len : int):
-    """Returns the vector with all complex exponential values used to calculate the DFT
-
-    Parameters
-    ----------
-    freq_idx : int
-        the index of the frequency bin
-    dft_len : int
-        the number of frequency bins
-    
-    Returns
-    -------
-    exp_vector : ndarray of shape (dft_len,)
-    """
-    exp_factor = -2 * np.pi * 1j * freq_idx / dft_len
-    return np.exp(np.arange(dft_len) * exp_factor)
-
-def idft_vector (freq_idx : int, dft_len : int):
-    """See documentation for dft_vector
-    """
-    exp_factor = 2 * np.pi * 1j * freq_idx / dft_len
-    return np.exp(np.arange(dft_len) * exp_factor) / dft_len
-
-
+import aspcol.fouriertransform as ft
 
 
 # ============== DESIGN FIR FILTERS ====================
+def fir_from_frequency_function(freq_function, ir_len, samplerate, window=None):
+    """Constructs a FIR filter from a frequency function
+
+    In order to make the filter casual, the impulse response is centered in the middle, meaning that an
+    extra ir_len // 2 samples of delay is added by the filter. To obtain the true response of freq_function, 
+    filter with the FIR filter and then shift the signal by ir_len // 2 samples.
+
+    If freq_function is real-valued the filter will be a linear-phase filter with a delay of ir_len // 2 samples.
+    
+    Parameters
+    ----------
+    freq_function : function
+        a function that takes a 1D array of frequencies as input and returns the complex frequency response
+    ir_len : int
+        the length of the impulse response. Currently only works for odd lengths.
+    samplerate : int
+        the sampling rate of the signal
+    
+    Returns
+    -------
+    ir : ndarray of shape (ir_len)
+    """
+    if ir_len % 2 == 0:
+       raise NotImplementedError("Function only implemented for odd impulse response lengths (will give incorrect group delay for even lengths)")
+
+    num_freqs = ir_len
+    freqs = ft.get_real_freqs(num_freqs, samplerate)
+    freq_response = freq_function(freqs)
+    freq_response[0] = np.real(freq_response[0])
+    if num_freqs % 2 == 0:
+        freq_response[-1] = np.real(freq_response[-1])
+
+    ir = ft.irfft(freq_response, n=num_freqs)
+    ir = np.real_if_close(ir)
+
+    mid_point = ir_len // 2 + 1
+    ir = np.concatenate((ir[...,mid_point:], ir[...,:mid_point]), axis=-1)
+
+    if window == "hamming":
+            ir = ir * signal.windows.hamming(ir_len).reshape(
+            (1,) * (ir.ndim - 1) + ir.shape[-1:]
+            )
+    elif window is not None:
+        raise NotImplementedError("Only Hamming supported currently.")
+    return ir
+
+
 
 def fir_from_freqs_window(freq_filter, ir_len, two_sided=True, window="hamming"):
     """Constructs a FIR filter from frequency values
@@ -178,9 +88,10 @@ def fir_from_freqs_window(freq_filter, ir_len, two_sided=True, window="hamming")
         #halfLen = irLen // 2
         mid_point = freq_filter.shape[0] // 2
 
-        time_filter = np.fft.ifft(freq_filter, axis=0)
-        new_axis_order = np.concatenate((np.arange(1, freq_filter.ndim), [0]))
-        time_filter = np.real_if_close(np.transpose(time_filter, new_axis_order))
+        time_filter = np.real_if_close(ft.ifft(freq_filter))
+        #time_filter = np.fft.ifft(freq_filter, axis=0)
+        #new_axis_order = np.concatenate((np.arange(1, freq_filter.ndim), [0]))
+        #time_filter = np.real_if_close(np.transpose(time_filter, new_axis_order))
 
         time_filter = np.concatenate((time_filter[...,-mid_point:], time_filter[...,:mid_point]), axis=-1)
 
@@ -309,9 +220,89 @@ def min_truncated_length(ir, two_sided=True, max_rel_trunc_error=1e-3):
 
 
 
+def fractional_delay_filter(delay, order):
+    """Returns impulse response of a fractional delay filter using lagrange interpolation
+
+    WARNING: currently untested for delays that are not between 0 and 1.
+
+    The factional delay filter h is non-causal, and adds an integer delay, which is returned as added_integer_delay.
+    To get the correct fractional delay after filtering, the signal should be extracted as
+    signal[n - delay] = (signal * h)[n - added_integer_delay]
+
+    Parameters
+    ----------
+    delay : float
+        the delay in samples
+    order : int
+        the interpolation order. 
+    
+    Returns
+    -------
+    ir : ndarray of shape (max_filt_len)
+        the impulse response of the filter
+    added_integer_delay : int
+        the integer delay added with the fractional delay filter
+
+    Notes
+    -----
+    An example that will correctly compensate for the added integer delay:
+    import scipy.signal as spsig
+    delay_filter, int_delay = fractional_delay_filter(samples_delay, filter_order)
+    delayed_cardioid = spsig.fftconvolve(delay_filter[None,:], signal, axes=-1)
+    delayed_cardioid = delayed_cardioid[...,int_delay:]
+    """
+    #ir = np.zeros(max_filt_len)
+    frac_dly, dly = np.modf(delay)
+    dly = int(dly)
+
+    #order = int(np.min((max_order, 2 * dly, 2 * (max_filt_len - 1 - dly))))
+
+    filt_len = 2*order + 1
+    delta = order + frac_dly
+    h = _lagrange_interpol(filt_len, delta)
+
+    added_integer_delay = order - dly
+
+    if dly > added_integer_delay:
+        raise NotImplementedError("Not correctly implemented for larger delays")
+
+    # diff = delay - delta
+    # start_idx = int(np.floor(diff))
+    # ir[start_idx : start_idx + order + 1] = h
+    return h, added_integer_delay
 
 
-# ===================== FILTERING FUNCTIONS ============================
+def _lagrange_interpol(N, delta):
+    """Lagrange interpolation
+    
+    Parameters
+    ----------
+    N : int
+        the order of the interpolation
+    delta : float
+        the fractional delay
+    
+    Returns
+    -------
+    h : ndarray of shape (N+1)
+        the impulse response of the filter
+    """
+    ir_len = int(N + 1)
+    h = np.zeros(ir_len)
+
+    for n in range(ir_len):
+        k = np.arange(ir_len)
+        k = k[np.arange(ir_len) != n]
+
+        h[n] = np.prod((delta - k) / (n - k))
+    return h
+
+
+
+
+
+
+# ===================== APPLIED FILTERING FUNCTIONS ============================
 def filterbank_third_octave(sig, sr, min_freq = 40, plot=False):
     """Filters the provided signal into third-octave bands using butterworth filters
     
@@ -373,3 +364,157 @@ def filterbank_third_octave(sig, sr, min_freq = 40, plot=False):
 
     sig_filtered = np.stack([signal.sosfiltfilt(sos[i], sig) for i in range(num_bands)], axis=0)
     return sig_filtered, freq_lims
+
+
+
+def differential_cardioid_microphone(signal_main, signal_secondary, microphone_distance, filter_order, c, samplerate, filter_below=None):
+    """Constructs a cardioid microphone response from two omnidirectional microphones
+    
+    The two omnidirectional microphones must be separated by a distance of microphone_distance.
+    The strongest response is in the direction of the main microphone from the secondary, and the null is in the opposite direction.
+
+    Parameters
+    ----------
+    signal_main : ndarray of shape (num_mics, num_samples)
+        the signal from the main microphone
+    signal_secondary : ndarray of shape (num_mics, num_samples)
+        the signal from the secondary microphone
+    microphone_distance : float
+        the distance between the microphones
+    filter_order : int
+        the order of the filter. The filter will have 2*filter_order + 1 taps. 
+    c : float
+        the speed of sound. 
+    samplerate : int
+        the sampling rate of the signals. 
+    filter_below : float or None
+        if not None, filters out the frequencies below this frequency, given in Hertz. The differential microphone
+        is problematic at low frequencies, and this can be used to avoid this.
+
+    Returns
+    -------
+    cardioid_signal : ndarray of shape (num_mics, num_samples)
+        the signal from the cardioid microphone
+
+    References
+    ----------
+    [benestyStudy2013] J. Benesty and J. Chen, Study and design of differential microphone arrays, vol. 6. in Springer Topics in Signal Processing, vol. 6. Springer, 2013.
+    """
+    num_freq = 4*filter_order # we need more or equal frequency samples compared to the ir_len
+    ir_len = 2*filter_order + 1
+    #wave_num = ft.get_real_wavenum(num_freq, samplerate, c)
+
+    def freq_response_main_mic(f):
+        wave_num = 2 * np.pi * f / c
+        wave_num[wave_num == 0] = 1e-10 # To avoid division by zero warnings. Not strictly necessary.
+        both_mic_factor = 1 / (1 - np.exp(-1j * 2 * wave_num * microphone_distance))
+        both_mic_factor[0] = 0
+        mic1_factor = np.conj(both_mic_factor)
+
+        if filter_below is not None:
+            num_freqs_to_filter = np.sum(f <= filter_below)
+            scaling = np.linspace(0, 1, num_freqs_to_filter) ** 4
+            mic1_factor[:num_freqs_to_filter] *= scaling
+        return mic1_factor
+    
+    def freq_response_secondary_mic(f):
+        wave_num = 2 * np.pi * f / c
+        wave_num[wave_num == 0] = 1e-10 # To avoid division by zero warnings. Not strictly necessary.
+
+        both_mic_factor = 1 / (1 - np.exp(-1j * 2 * wave_num * microphone_distance))
+        both_mic_factor[0] = 0
+        mic2_factor = -np.conj(both_mic_factor * np.exp(-1j * wave_num * microphone_distance))
+
+        if filter_below is not None:
+            num_freqs_to_filter = np.sum(f <= filter_below)
+            scaling = np.linspace(0, 1, num_freqs_to_filter) ** 4
+            mic2_factor[:num_freqs_to_filter] *= scaling
+        return mic2_factor
+    
+    mic1_ir = fir_from_frequency_function(freq_response_main_mic, ir_len, samplerate, window=None)
+    mic2_ir = fir_from_frequency_function(freq_response_secondary_mic, ir_len, samplerate, window=None)
+
+    #mic1_factor = ft.insert_negative_frequencies(mic1_factor, even = True)
+    #mic2_factor = ft.insert_negative_frequencies(mic2_factor, even = True)
+
+    #mic1_ir, _ = fir_from_freqs_window(mic1_factor, ir_len, two_sided=True, window="hamming")
+    #mic2_ir, _ = fir_from_freqs_window(mic2_factor, ir_len, two_sided=True, window="hamming")
+
+    signal_main_filtered = signal.fftconvolve(mic1_ir[None,:], signal_main, axes=-1, mode="full")
+    signal_secondary_filtered = signal.fftconvolve(mic2_ir[None,:], signal_secondary, axes=-1, mode="full")
+
+    cardioid_signal = signal_main_filtered + signal_secondary_filtered
+    cardioid_signal = cardioid_signal[...,filter_order:filter_order+signal_main.shape[-1]]
+    return cardioid_signal
+
+
+
+
+
+# def differential_cardioid_microphone(signal_main, signal_secondary, microphone_distance, filter_order, c, samplerate, filter_below=None):
+#     """Constructs a cardioid microphone response from two omnidirectional microphones
+    
+#     The two omnidirectional microphones must be separated by a distance of microphone_distance.
+#     The strongest response is in the direction of the main microphone from the secondary, and the null is in the opposite direction.
+
+#     Parameters
+#     ----------
+#     signal_main : ndarray of shape (num_mics, num_samples)
+#         the signal from the main microphone
+#     signal_secondary : ndarray of shape (num_mics, num_samples)
+#         the signal from the secondary microphone
+#     microphone_distance : float
+#         the distance between the microphones
+#     filter_order : int
+#         the order of the filter. The filter will have 2*filter_order + 1 taps. 
+#     c : float
+#         the speed of sound. 
+#     samplerate : int
+#         the sampling rate of the signals. 
+#     filter_below : float or None
+#         if not None, filters out the frequencies below this frequency, given in Hertz. The differential microphone
+#         is problematic at low frequencies, and this can be used to avoid this.
+
+#     Returns
+#     -------
+#     cardioid_signal : ndarray of shape (num_mics, num_samples)
+#         the signal from the cardioid microphone
+
+#     References
+#     ----------
+#     [benestyStudy2013] J. Benesty and J. Chen, Study and design of differential microphone arrays, vol. 6. in Springer Topics in Signal Processing, vol. 6. Springer, 2013.
+#     """
+#     num_freq = 4*filter_order # we need more or equal frequency samples compared to the ir_len
+#     ir_len = 2*filter_order + 1
+#     wave_num = ft.get_real_wavenum(num_freq, samplerate, c)
+
+#     #def freq_response_main_mic(f):
+#     both_mic_factor = 1 / (1 - np.exp(-1j * 2 * wave_num * microphone_distance))
+#     both_mic_factor[0] = 0
+#     mic1_factor = np.conj(both_mic_factor)
+#     mic2_factor = -np.conj(both_mic_factor * np.exp(-1j * wave_num * microphone_distance))
+
+#     if filter_below is not None:
+#         #def freq_drop_function(freq):
+#         #    return 1 - 1 / (1 + (freq / filter_below) ** 4)
+        
+#         freqs = ft.get_real_freqs(num_freq, samplerate)
+#         num_freqs_to_filter = np.sum(freqs <= filter_below)
+#         #freqs_to_filter = freqs[freqs_to_filter_mask]
+#         scaling = np.linspace(0, 1, num_freqs_to_filter)
+#         scaling = scaling ** 4
+#         mic1_factor[:num_freqs_to_filter] *= scaling
+#         mic2_factor[:num_freqs_to_filter] *= scaling
+
+#     mic1_factor = ft.insert_negative_frequencies(mic1_factor, even = True)
+#     mic2_factor = ft.insert_negative_frequencies(mic2_factor, even = True)
+
+#     mic1_ir, _ = fir_from_freqs_window(mic1_factor, ir_len, two_sided=True, window="hamming")
+#     mic2_ir, _ = fir_from_freqs_window(mic2_factor, ir_len, two_sided=True, window="hamming")
+
+#     signal_main_filtered = signal.fftconvolve(mic1_ir[None,:], signal_main, axes=-1, mode="full")
+#     signal_secondary_filtered = signal.fftconvolve(mic2_ir[None,:], signal_secondary, axes=-1, mode="full")
+
+#     cardioid_signal = signal_main_filtered + signal_secondary_filtered
+#     cardioid_signal = cardioid_signal[...,filter_order:filter_order+signal_main.shape[-1]]
+#     return cardioid_signal
