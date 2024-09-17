ba6347cc691796da003bacde8c54110f6ea8954c
Jesper Brunnstroem <jbrunnst@esat.kuleuven.ac.be>
2024-09-17 12:01:39 +0200

    cleaned up examples

--- examples/soundfieldestimation_rir_circular_differential_cardioid.py
+++ examples/soundfieldestimation_rir_circular_differential_cardioid.py
@@ -14,20 +14,17 @@ import aspcol.movingmicrophone as mm
 import aspcol.sphericalharmonics as shd
 import aspcol.plot as aspplot   
 
-import exp_funcs_ideal_sampling as exis
+import _exp_funcs_ideal_sampling as exis
 import sys
 
 sys.path.append("C:/research/papers/2024_icassp_moving_mic_sf_estimation/code")
 import plots as fplot
-import hahn_spors_estimation as hse
 
 
 def run_exp(fig_folder):
     sig, sim_info, arrays, pos_dyn, seq_len, extra_params, pos_dyn_cardioid, mic_direction = exis.load_session(fig_folder)
 
     print(f"Simulation start position: {pos_dyn[0,0,:]}, in polar coords: {gp.cart2pol(pos_dyn[0,0,0], pos_dyn[0,0,1])}")
-    #print(f"Start position after init delay: {pos_dyn[extra_params['initial_delay'],0,:]}, in polar coords: {gp.cart2pol(pos_dyn[extra_params['initial_delay'],0,0], pos_dyn[extra_params['initial_delay'],0,1])}")
-
     p = sig["mic"][:,-seq_len:]
     p_eval = sig["eval"][:,-seq_len:]
     p_dyn = sig["mic_dynamic"]
@@ -44,7 +41,6 @@ def run_exp(fig_folder):
 
     num_freqs = seq_len // num_ls
     real_freqs = ft.get_real_freqs(num_freqs, sim_info.samplerate)
-    num_real_freqs = real_freqs.shape[-1]
 
     rir_eval = arrays.paths["src"]["eval"]
     rir_eval_freq = np.moveaxis(ft.rfft(rir_eval), 1, 2)
@@ -53,7 +49,6 @@ def run_exp(fig_folder):
     reg_param_td = 1e-8
     
     estimates = {}
-    diag = {}
 
     sequence = sequence[:seq_len]
 
@@ -67,34 +62,15 @@ def run_exp(fig_folder):
     rir_eval_freq = rir_eval_freq[::ds_factor,...]
     real_freqs = real_freqs[::ds_factor]
 
-    #arrays["eval"].pos = arrays["eval"].pos[::10,:]
-    #rir_eval_freq = rir_eval_freq[:,::10,:]
-
-
     dir_coeffs = shd.directivity_linear(0.5, mic_direction)[None,:,:] #* np.ones((pos_dyn.shape[0], 1))
     estimates["cardioid jax"] = mm.inf_dimensional_shd_dynamic_compiled(p_dyn_cardioid, pos_dyn_cardioid, arrays["eval"].pos, sequence, sim_info.samplerate, sim_info.c, reg_param_td, dir_coeffs)
+    estimates["proposed omni"] = sfe.est_inf_dimensional_shd_dynamic(p_dyn, pos_dyn, arrays["eval"].pos, sequence, sim_info.samplerate, sim_info.c, reg_param_td)
 
-    #estimates["cardioid numpy"] = mm.inf_dimensional_shd_dynamic(p_dyn_cardioid, pos_dyn_cardioid, arrays["eval"].pos, sequence, sim_info.samplerate, sim_info.c, #reg_param_td, dir_coeffs)
+    estimates["nearest neighbour"] = sfe.pseq_nearest_neighbour(p, sequence[:seq_len], arrays["mic"].pos, arrays["eval"].pos)
+    estimates["kernel interpolation"] = sfe.est_ki_diffuse(p, sequence[:seq_len], arrays["mic"].pos, arrays["eval"].pos, sim_info.samplerate, sim_info.c, reg_param)
 
-    #dir_coeffs = shd.directivity_omni() * np.ones((pos_dyn.shape[0], 1))
-    #estimates["proposed omni"] = mm.inf_dimensional_shd_dynamic(p_dyn, pos_dyn, arrays["eval"].pos, sequence, sim_info.samplerate, sim_info.c, reg_param_td, dir_coeffs)
-
-    estimates["proposed"] = sfe.est_inf_dimensional_shd_dynamic(p_dyn, pos_dyn, arrays["eval"].pos, sequence, sim_info.samplerate, sim_info.c, reg_param_td)
-    #print(f"MSE between the two versions: {np.mean(np.abs(estimates['cardioid jax'] - estimates['cardioid numpy'])**2)}")
-    # print(f"MSE between the omni and cardioid: {np.mean(np.abs(estimates['proposed cardioid'] - estimates['proposed'])**2) / np.mean(np.abs(estimates['proposed'])**2)}")
-    
-    #estimates["nearest neighbour"] = sfe.pseq_nearest_neighbour(p, sequence[:seq_len], arrays["mic"].pos, arrays["eval"].pos)
-    #estimates["kernel interpolation"] = sfe.est_ki_diffuse(p, sequence[:seq_len], arrays["mic"].pos, arrays["eval"].pos, sim_info.samplerate, sim_info.c, reg_param)
-    #estimates["1d-interpolation"]  = hse.est_rir_circle(p_dyn, pos_dyn, arrays["eval"].pos, sequence[:seq_len], sim_info.samplerate, center, int_order=512)
-    
-    #print(f"MSE new version: {np.mean((p_eval - estimates['proposed omni'])**2)}")
-    #print(f"MSE hardcoded omni version: {np.mean((p_eval - estimates['proposed'])**2)}")
-    # dir_coeffs = shd.directivity_linear(0.5, mic_direction)
-    # estimates["proposed cardioid"] = sfe.est_inf_dimensional_shd_dynamic(p_dyn_cardioid, pos_dyn_cardioid, arrays["eval"].pos, sequence[:seq_len], sim_info.samplerate, sim_info.c, reg_param_td, dir_coeffs)
 
     fplot.soundfield_estimation_comparison(arrays, estimates, rir_eval_freq, real_freqs, fig_folder, shape="circle", center=center, num_ls=num_ls, output_method=OUTPUT_METHOD)
-    #if num_ls == 1:
-    #    test_pseq.run_from_data(p, sequence, seq_len, sim_info.samplerate, extra_params["max_sweep_freq"],arrays, fig_folder, sim_info, extra_params)
 
 
 def highpass_microphone_signal(mic_sig, filter_order, samplerate, filter_below):
@@ -182,22 +158,6 @@ def test_differential_cardioid_generates_same_data_as_pyroomacoustics_for_far_aw
 if __name__ == "__main__":
     OUTPUT_METHOD = "pdf"
     sr = 1000
-    #multi_speed_experiment(sr)
-    #fig_folder = exis.generate_signals_circular_updownsample(800, 2, 1)
     fig_folder = exis.generate_signals_circular_differential_microphone(sr, 1, 10)
-    #fig_folder = exis.generate_signals_circular(sr, 1, 4)
-    #fig_folder = pathlib.Path("c:/research/code/aspcol/examples/figs/figs_2024_06_26_14_49_0")
-    #run_exp(fig_folder)
-    #fig_folder = exis.generate_signals_circular(sr, 2, 20)
-    #run_exp(fig_folder)
-    #fig_folder = pathlib.Path("c:/research/research_documents/202305_moving_mic_spatial_cov_estimation/code/figs/figs_2023_08_02_12_44_0")
-    #fig_folder = pathlib.Path("c:/research/research_documents/202305_moving_mic_spatial_cov_estimation/code/figs/figs_2023_07_04_15_31_0")
-    
-
-    #from pyinstrument import Profiler
-    #profiler = Profiler()
-    #profiler.start()
     run_exp(fig_folder)
 
-    #profiler.stop()
-    #profiler.print()
