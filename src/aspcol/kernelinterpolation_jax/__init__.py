23ea42e5cdc5b094aa32d9978cf3adbee8f0b3b5
Jesper Brunnstroem <jbrunnst@esat.kuleuven.ac.be>
2025-09-17 10:30:08 +0200

    unified naming of kernel functions

--- src/aspcol/kernelinterpolation_jax/__init__.py
+++ src/aspcol/kernelinterpolation_jax/__init__.py
@@ -1,4 +1,4 @@
 from aspcol.kernelinterpolation_jax.kernel import *
 from aspcol.kernelinterpolation_jax.kernel_multifreq import *
-from aspcol.kernelinterpolation_jax.kernel_multisource import *
-from aspcol.kernelinterpolation_jax.kernel_timedomain import *
+from aspcol.kernelinterpolation_jax.kernel_multisrc import *
+from aspcol.kernelinterpolation_jax.kernel_time_domain import *
