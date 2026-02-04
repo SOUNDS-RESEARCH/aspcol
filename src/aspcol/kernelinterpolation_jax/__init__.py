d29170f3e860a72d72e864a4950ebecf8a763bd6
Jesper Brunnstroem <jbrunnst@esat.kuleuven.ac.be>
2026-02-04 18:28:40 +0100

    imports

--- src/aspcol/kernelinterpolation_jax/__init__.py
+++ src/aspcol/kernelinterpolation_jax/__init__.py
@@ -2,3 +2,4 @@ from aspcol.kernelinterpolation_jax.kernel import *
 from aspcol.kernelinterpolation_jax.kernel_multifreq import *
 from aspcol.kernelinterpolation_jax.kernel_multisrc import *
 from aspcol.kernelinterpolation_jax.kernel_time_domain import *
+from aspcol.kernelinterpolation_jax.cross_validation import *
